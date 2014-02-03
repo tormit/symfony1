97d72c862e7b038ea6caf358637ab1523d9f30b2
Tormi Talv <tormit@gmail.com>
2014-02-03 19:07:40 +0200

    generate:module task now generates default components class also

---
+++ lib/task/generator/skeleton/module/module/actions/components.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * ##MODULE_NAME## components.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage ##MODULE_NAME##
+ * @author     ##AUTHOR_NAME##
+ * @version    SVN: $Id: components.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ##MODULE_NAME##Components extends sfComponents
+{
+    public function execute##COMPONENT_ACTION##()
+    {
+
+    }
+}
